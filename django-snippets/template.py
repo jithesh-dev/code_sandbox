4f0c9880a84707ac17ff12002722cc557b9dffa9
jithesh-dev <62261060+jithesh-dev@users.noreply.github.com>
2020-03-31 11:27:57 +0530

    Add files via upload

---
+++ django-snippets/template.py
@@ -0,0 +1,11 @@
+"""
+Usage:
+  view:
+  some_dict = {'keyA':'valueA','keyB':{'subKeyA':'subValueA','subKeyB':'subKeyB'},'keyC':'valueC'}
+  keys = ['keyA','keyC']
+  template:
+  {{ some_dict|get:"keyA" }}
+  {{ some_dict|get:"keyB"|get:"subKeyA" }}
+  {% for key in keys %}{{ some_dict|get:key }}{% endfor %}
+
+"""
